11a30714ad999679749e2b5139b6c46ae737129a
lynthithu-oss <lynthithu@gmail.com>
2025-10-08 16:32:15 +0630

    Add files via upload

---
+++ new.js
@@ -0,0 +1,13 @@
+function copyText() {
+    var copyText = document.getElementById("copyText");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    document.execCommand("copy");
+
+    // Alert the copied text
+    alert("Text copied to clipboard: " + copyText.value);
+}
